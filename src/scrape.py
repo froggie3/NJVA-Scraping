3925d5a3897df666ff14506ba4538a5fdee0e08b
Yokkin <76399182+froggie3@users.noreply.github.com>
2023-01-22 09:34:45 +0900

    Better error handling

--- src/scrape.py
+++ src/scrape.py
@@ -9,23 +9,45 @@ import requests
 import shutil
 import time
 
+
+class HelpFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                    argparse.RawTextHelpFormatter,
+                    argparse.MetavarTypeHelpFormatter):
+    pass
+
+
 parser = argparse.ArgumentParser(
+    description='A script that retrieves all the thread archives from the URLs written in a specified JSON file.',
+    epilog='',
+    formatter_class=HelpFormatter,
     prog='JNVA-Scraping',
-    description='',
-    epilog=''
 )
 
-# parser.add_argument('-s', '--sleep',
-#                     default=5,
-#                     help='specify the interval at which you download each HTML',
-#                     metavar='N',
-#                     required=False,
-#                     type=int,
-#                     )
+parser.add_argument('-s', '--sleep',
+                    choices=range(3, 10),
+                    default=5,
+                    help='specify the interval at which you download each HTML\n',
+                    metavar='[integer]',
+                    nargs=1,
+                    required=False,
+                    type=int,
+                    )
+
 parser.add_argument('-j', '--json',
-                    help='choose a path for a JSON file containing thread URLs and titles',
-                    metavar='PATH',
-                    required=True,
+                    default='./target/latest.json',
+                    help='choose a path for a JSON file containing thread URLs and titles\n',
+                    metavar='[path]',
+                    nargs=1,
+                    required=False,
+                    type=str,
+                    )
+
+parser.add_argument('-o', '--outdir',
+                    default='./downloads/html/',
+                    help='specify the directory in which the archive HTML files to be saved\n',
+                    metavar='[directory]',
+                    nargs=1,
+                    required=False,
                     type=str,
                     )
 
@@ -33,39 +55,51 @@ args = parser.parse_args()
 
 
 def main():
+    # JSON ファイルの場所を指定
     json_path = args.json
     old_json_path = json_path.replace('.json', '.old.json')
 
-    # JSONの存在を確認し、配列を作成
-    if os.path.exists(path=json_path):
-        thread_list = jsonLoader(json_path)
+    # 休憩時間
+    sleep_time = args.sleep
 
-        # .old.json の存在も確認し、無かったらコピーを作成し配列に代入
-        if not os.path.exists(path=old_json_path):
-            print(color('%s was not found. Making a copy of JSON file...' %
-                        old_json_path, fore='yellow'))
-            shutil.copyfile(json_path, old_json_path)
-
-            # 作成してもなかったら処理を中断
-            if not os.path.exists(path=old_json_path):
-                exit()
-        old_thread_list = jsonLoader(old_json_path)
-    else:
-        print(color('%s was not found!' % json_path, fore='red'))
+    # 保存先となるディレクトリの指定
+    out_dir = args.outdir.strip('/').strip('\\') + '/'
+    if not os.path.exists(path=out_dir):
+        print(color('[FATAL] %s is not a valid path for a JSON File.' %
+                    args.outdir, fore='red'))
         exit()
 
-    # 最新のスレッドは基本1000まで埋まっていないので、再ダウンロードしてもらう
-    # latest.old.json から当時「最新」だった HTML の名前を特定し、削除
-    last_thread_path = './downloads/html/' + \
-        old_thread_list[0]['thread_title'] + '.html'
+    # JSONの存在を確認し、配列を作成
+    thread_list = jsonLoader(json_path)
+
+    # .old.json の存在も確認 (なかったらコピーを試行)
+    retry_interval = 2
+    tries = 2
+    for i in range(0, tries):
+        if not os.path.exists(path=old_json_path):
+            if i == 0:
+                print(color('[WARN] %s was not found. Making a copy of JSON file...' %
+                            old_json_path, fore='yellow'))
+                shutil.copyfile(json_path, old_json_path)
+                time.sleep(retry_interval)
+            if i >= 1:
+                print(color('[FATAL] Failed when making a copy of %s.' %
+                            old_json_path, fore='red'))
+                exit()
+
+    old_thread_list = jsonLoader(old_json_path)
+
+    # latest.old.json から取得以前で最新の HTML の名前を特定
+    last_thread_path = out_dir + old_thread_list[0]['thread_title'] + '.html'
+
+    # 最新のスレッドは基本埋まっていないので、削除して再ダウンロードにまわす
     if (os.path.exists(last_thread_path)):
         os.remove(last_thread_path)
 
     for thread in thread_list:
-        if not os.path.exists(path='./downloads/html/'):
-            os.mkdir(path='./downloads/html/')
+        os.makedirs(name=out_dir, exist_ok=True)
 
-        export_path = './downloads/html/' + thread['thread_title'] + '.html'
+        export_path = out_dir + thread['thread_title'] + '.html'
 
         if not os.path.exists(path=export_path):
             # リクエストを送信
@@ -80,20 +114,23 @@ def main():
                 t = t.replace('charset=Shift_JIS', 'charset="UTF-8"')
                 fp.write(t)
 
-            print('[INFO] Exported to %s' % export_path)
-            time.sleep(2)
-
+            print(color('[INFO] Exported to %s' % export_path, fore='blue'))
+            time.sleep(sleep_time)
         else:
             print('[INFO] Skipped saving to %s' % export_path)
 
-    print(color('[INFO] Download finished', fore='blue'))
+    print(color('[INFO] Downloading finished', fore='blue'))
 
 
-def jsonLoader(path="target/latest.json"):
+def jsonLoader(path):
     # JSON を読んで配列を返す
-    print(color('%s was found!' % path, fore='blue'))
-    with open(path, 'r', encoding="utf-8") as fp:
-        return json.loads(fp.read())
+    if os.path.exists(path=path):
+        print(color('Found %s' % path, fore='blue'))
+        with open(path, 'r', encoding="utf-8") as fp:
+            return json.loads(fp.read())
+    else:
+        print(color('%s was not found!' % path, fore='red'))
+        exit()
 
 
 if __name__ == '__main__':
